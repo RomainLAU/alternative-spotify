84325660a55547acb87a97e5d426460eb2ea6050
RomainLAU <romain.laurent@epitech.digital>
2022-11-10 12:54:21 +0100

    Add search artist function

--- src/api/spotify.ts
+++ src/api/spotify.ts
@@ -1,14 +1,26 @@
 import { instance } from './index';
 
 export function search(query: string, type: string, limit = 10) {
-  return instance.get('/search', {
-    params: {
-      q: query,
-      type,
-      limit,
-    }
-  })
-  .then(response => {
-    return response.data;
-  })
+  return instance
+    .get('/search', {
+      params: {
+        q: query,
+        type,
+        limit,
+      },
+    })
+    .then((response) => {
+      return response.data;
+    });
+}
+
+export async function searchArtist(query: string, type = 'artist') {
+  const response = await search(query, type);
+  const artistId = response.artists.items[0].id;
+
+  return instance
+    .get(`https://api.spotify.com/v1/artists/${artistId}`)
+    .then((response) => {
+      return response.data;
+    });
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { search } from '@/api/spotify';
+import { search, searchArtist } from '@/api/spotify';
 import { ref } from 'vue';
 
 const response: Array<any> = ref(null);
 
 async function init() {
-  response.value = await search('ziak', 'artist');
+  response.value = await searchArtist('Imagine Dragons');
+  console.log(response.value);
 }
 
 init();
@@ -13,13 +14,9 @@ init();
 
 <template>
   <main class="p-16 bg-[#E1E9ED]">
-    <img
-      v-if="response !== null"
-      :src="response.artists.items[0].images[0].url"
-      alt=""
-    />
-    <h1 v-if="response !== null" class="">
-      {{ response.artists.items[0].name }}
+    <h1 v-if="response !== null" class="text-9xl font-extrabold">
+      {{ response.name }}
     </h1>
+    <img v-if="response !== null" :src="response.images[0].url" alt="" />
   </main>
 </template>
