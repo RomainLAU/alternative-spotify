20d7f6d3c18739fb80d7f58c947d641f180d52b7
RomainLAU <romain.laurent@epitech.digital>
2022-11-10 11:40:02 +0100

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { search } from '@/api/spotify';
+import { ref } from 'vue';
+
+const artists = ref([]);
+
+async function init() {
+  artists.value = await search('ziak', 'artist');
+}
+
+init();
+
+console.log(artists);
+
+// let image = artists.value.items[0].images[0];
+</script>
+
+<template>
+  <main class="p-16 bg-[#E1E9ED]">
+    <img :href="artists.items[0].images[0]" alt="" />
+    <pre>{{ artists }}</pre>
+  </main>
+</template>
