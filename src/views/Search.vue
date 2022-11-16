1c96e6919598e6afe95388ce56b3e9ea6ce36dde
RomainLAU <romain.laurent@epitech.digital>
2022-11-16 16:49:54 +0100

    Show images from the found albums

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,5 +1,21 @@
 <script setup lang="ts">
+import { search } from '@/api/spotify';
 import SearchBar from '@/components/Searchbar.vue';
+import type { SearchAlbums } from 'types/album';
+import { ref } from 'vue';
+import { useRoute, type RouteLocationNormalizedLoaded } from 'vue-router';
+
+const route: RouteLocationNormalizedLoaded = useRoute();
+
+const albums = ref<SearchAlbums | null>(null);
+
+async function init() {
+  if (route.query.query) {
+    albums.value = (await search(route.query.query, 'album')).albums;
+  }
+}
+
+init();
 </script>
 
 <template>
@@ -7,5 +23,13 @@ import SearchBar from '@/components/Searchbar.vue';
     class="container-xl h-screen bg-[#0f172a] text-white flex flex-col p-16"
   >
     <SearchBar />
+    <div v-if="albums">
+      <ul class="flex gap-7 mt-12">
+        <li v-for="album in albums?.items" :key="album.id">
+          <img :src="album.images[1].url" alt="album.name" />
+        </li>
+      </ul>
+    </div>
+    <p v-else>No query</p>
   </main>
 </template>
